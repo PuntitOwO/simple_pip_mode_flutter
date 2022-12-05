d72df7a496bf5a4168e97d2b7d9bddae5cb74ce1
Erick Daros Silva <erickdarosilva@gmail.com>
2022-12-04 23:36:34 -0300

    Implement setPipLayout, setIsPlaying and onPipAction callback

--- android/src/main/kotlin/cl/puntito/simple_pip_mode/PipCallbackHelper.kt
+++ android/src/main/kotlin/cl/puntito/simple_pip_mode/PipCallbackHelper.kt
@@ -1,6 +1,7 @@
 package cl.puntito.simple_pip_mode
 
 import androidx.annotation.NonNull
+import cl.puntito.simple_pip_mode.actions.PipAction
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.embedding.engine.FlutterEngine
 
@@ -13,11 +14,19 @@ open class PipCallbackHelper {
     channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
   }
 
+  fun setChannel(channel: MethodChannel) {
+    this.channel = channel
+  }
+
   fun onPictureInPictureModeChanged(active: Boolean) {
     if (active) {
       channel.invokeMethod("onPipEntered", null)
     } else {
       channel.invokeMethod("onPipExited", null)
     }
+  }
+
+  fun onPipAction(action: PipAction) {
+    channel.invokeMethod("onPipAction", action.name.lowercase())
   }
 }
