3eb9c2e6701df029cc58e3ff37695893625c364c
Erick Daros Silva <erickdarosilva@gmail.com>
2022-12-05 01:09:47 -0300

    Extract string resource

--- android/src/main/kotlin/cl/puntito/simple_pip_mode/actions/PipAction.kt
+++ android/src/main/kotlin/cl/puntito/simple_pip_mode/actions/PipAction.kt
@@ -13,21 +13,21 @@ import cl.puntito.simple_pip_mode.R
 
 enum class PipAction(
     private val icon: Int,
-    private val title: String,
-    private val description: String,
+    private val title: Int,
+    private val description: Int,
     private val afterAction: String? = null,
 ) {
-    PLAY(R.drawable.ic_baseline_play_arrow_24, "Play", "Play media", "PAUSE"),
-    PAUSE(R.drawable.ic_baseline_pause_24, "Pause", "Pause media", "PLAY"),
-    NEXT(R.drawable.ic_baseline_skip_next_24, "Next", "Skip to next media"),
-    PREVIOUS(R.drawable.ic_baseline_skip_previous_24, "Previous", "Back to previous media"),
-    LIVE(R.drawable.ic_surround_sound_24, "Live", "Go to live");
+    PLAY(R.drawable.ic_baseline_play_arrow_24, R.string.pip_action_play, R.string.pip_action_play_description, "PAUSE"),
+    PAUSE(R.drawable.ic_baseline_pause_24, R.string.pip_action_pause, R.string.pip_action_pause_description, "PLAY"),
+    NEXT(R.drawable.ic_baseline_skip_next_24, R.string.pip_action_next, R.string.pip_action_next_description),
+    PREVIOUS(R.drawable.ic_baseline_skip_previous_24, R.string.pip_action_previous, R.string.pip_action_previous_description),
+    LIVE(R.drawable.ic_surround_sound_24, R.string.pip_action_live, R.string.pip_action_live_description,);
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun toRemoteAction(context: Context) : RemoteAction = RemoteAction(
         Icon.createWithResource(context, icon),
-        title,
-        description,
+        context.getString(title),
+        context.getString(description),
         PendingIntent.getBroadcast(
             context, ordinal,
             Intent(SIMPLE_PIP_ACTION).putExtra(EXTRA_ACTION_TYPE, name),
