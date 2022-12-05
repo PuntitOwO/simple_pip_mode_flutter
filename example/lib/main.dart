478d4a271cb14beea522285f7dd468a9f52526cb
Erick Daros Silva <erickdarosilva@gmail.com>
2022-12-05 00:59:45 -0300

    Update example with PIP Actions examples

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:simple_pip_mode/actions/pip_action.dart';
+import 'package:simple_pip_mode/actions/pip_actions_layout.dart';
 import 'dart:async';
 
 import 'package:simple_pip_mode/simple_pip.dart'; // To enter pip mode and receive callbacks
@@ -34,6 +36,11 @@ class _ExampleAppState extends State<ExampleApp> {
   bool autoPipSwitch = false;
   late SimplePip pip;
 
+  PipActionsLayout pipActionsLayout = PipActionsLayout.none;
+  // Used to represent interaction with PIP actions
+  bool isPlaying = true;
+  String actionResponse = "";
+
   @override
   void initState() {
     super.initState();
@@ -52,12 +59,63 @@ class _ExampleAppState extends State<ExampleApp> {
     });
   }
 
+  List<DropdownMenuItem<PipActionsLayout>> layoutList() {
+    return PipActionsLayout.values
+        .map<DropdownMenuItem<PipActionsLayout>>(
+          (PipActionsLayout value) => DropdownMenuItem<PipActionsLayout>(
+            child: Text(value.name),
+            value: value,
+          ),
+        )
+        .toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // Pip widget can build different widgets for each mode
       home: PipWidget(
         // builder is null so child is used when not in pip mode
+        pipLayout: pipActionsLayout,
+        onPipAction: (action) {
+          print("PIP ACTION TAP: " + action.name);
+          switch (action) {
+            case PipAction.play:
+              // example: videoPlayerController.play();
+              setState(() {
+                isPlaying = true;
+                actionResponse = "Playing";
+              });
+              break;
+            case PipAction.pause:
+              // example: videoPlayerController.pause();
+              setState(() {
+                isPlaying = false;
+                actionResponse = "Paused";
+              });
+              break;
+            case PipAction.live:
+              // example: videoPlayerController.forceLive();
+              setState(() {
+                actionResponse = "Go to live view";
+              });
+              break;
+            case PipAction.next:
+              // example: videoPlayerController.next();
+              setState(() {
+                actionResponse = "Next";
+              });
+              break;
+            case PipAction.previous:
+              // example: videoPlayerController.next();
+              setState(() {
+                actionResponse = "Previous";
+              });
+              break;
+            default:
+              break;
+          }
+        },
         child: Scaffold(
           appBar: AppBar(
             title: const Text('Pip Plugin example app'),
@@ -116,6 +174,64 @@ class _ExampleAppState extends State<ExampleApp> {
                     : null,
                 icon: const Icon(Icons.picture_in_picture),
               ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Divider(
+                  thickness: 1,
+                ),
+              ),
+              const Text("PIP Actions:"),
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text("Current actions layout: "),
+                    DropdownButton<PipActionsLayout>(
+                      value: pipActionsLayout,
+                      onChanged: (PipActionsLayout? newValue) {
+                        if (newValue == null) return;
+                        pip.setPipActionsLayout(newValue);
+                        pip.setIsPlaying(true);
+                        setState(() {
+                          isPlaying = true;
+                          pipActionsLayout = newValue;
+                        });
+                      },
+                      items: layoutList(),
+                    ),
+                  ],
+                ),
+              ),
+              pipActionsLayout != PipActionsLayout.none
+                  ? Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              const Text("Simulated player: "),
+                              IconButton(
+                                  onPressed: () {
+                                    bool newValue = !isPlaying;
+                                    pip.setIsPlaying(newValue);
+                                    setState(() {
+                                      isPlaying = newValue;
+                                    });
+                                  },
+                                  icon: Icon(
+                                      isPlaying ? Icons.pause : Icons.play_arrow))
+                            ],
+                          ),
+                      ),
+                        const Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Text("Obs.: Tap the simulated player button to see the PIP actions be updated on PIP mode, when you tap PIP actions on PIP mode it will reflect here too"),
+                        )
+                    ],
+                  )
+                  : Container(),
             ],
           ),
         ),
@@ -127,9 +243,23 @@ class _ExampleAppState extends State<ExampleApp> {
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              SizedBox(width: double.infinity),
-              Text('Pip activated'),
+            children: [
+              const SizedBox(width: double.infinity),
+              const Text('Pip activated'),
+              pipActionsLayout != PipActionsLayout.none
+                  ? IconButton(
+                      onPressed: () {
+                        bool newValue = !isPlaying;
+                        pip.setIsPlaying(newValue);
+                        setState(() {
+                          isPlaying = newValue;
+                        });
+                      },
+                      icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow))
+                  : Container(),
+              pipActionsLayout != PipActionsLayout.none
+                  ? Text(actionResponse)
+                  : Container(),
             ],
           ),
         ),
