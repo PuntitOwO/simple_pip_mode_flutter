8631b0e4fb2b4805cb0d2d5ad52f85f58d8767f3
PuntitOwO <christopher.marin@ug.uchile.cl>
2025-01-23 19:25:14 -0300

    docs: add documentation to AspectRatio

--- lib/aspect_ratio.dart
+++ lib/aspect_ratio.dart
@@ -1,8 +1,17 @@
+/// AspectRatio type to represent the width to height ratio.
 typedef AspectRatio = (int, int);
 
+/// Extension to get the width, height, name, and list form of an AspectRatio.
 extension AspectRatioExtension on AspectRatio {
+  /// Width of the aspect ratio.
   int get width => this.$1;
+
+  /// Height of the aspect ratio.
   int get height => this.$2;
+
+  /// Aspect ratio in a human readable form.
   String get name => '$width:$height';
+
+  /// Aspect ratio as a list.
   List<int> get asList => <int>[width, height];
 }
